0a01b63b91ab59408fe5527511625c8337198647
Will Baumbach <william.baumbach@uconn.edu>
2020-04-21 22:23:19 -0400

    fix pls

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static('./dist/{{your-app-name}}'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/cardWorkout/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/cardWorkout/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
