9a170a49536cb0519955f53ae618184c8c3ce1cc
Will Baumbach <william.baumbach@uconn.edu>
2020-04-21 22:32:37 -0400

    please work!!

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+app.use(express.static('./dist/cardWorkout'));
 
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname,'/dist/cardWorkout/index.html'));
